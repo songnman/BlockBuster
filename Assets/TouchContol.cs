7fe1b60238b4b019fd66ba6d5ce8c78434af40ad
Jerry <songnman@gmail.com>
2019-03-07 18:37:00 +0900

    공 던지는 방향 설정 테스트

--- Assets/TouchContol.cs
+++ Assets/TouchContol.cs
@@ -7,29 +7,41 @@ public class TouchContol : MonoBehaviour, IPointerDownHandler
 {
 	public GameObject ballObj;
 	public BottomWall bottomwallSc;
+	private LineRenderer line;
 	void Start()
 	{
-
+		line = GetComponent<LineRenderer>();
 	}
 	public void OnPointerDown(PointerEventData eventData)
 	{
 		// Debug.Log("Touch!");
-		if(bottomwallSc.isBallStickBottom)
-			ballObj.GetComponent<Rigidbody2D>().AddForce(new Vector2(Random.Range(-500,500),Random.Range(500,800)) );
+		// if(bottomwallSc.isBallStickBottom)
+		// 	ballObj.GetComponent<Rigidbody2D>().AddForce(new Vector2(Random.Range(-500,500),Random.Range(500,800)) );
 	}
-	private void HandleTouch(int touchFingerId, Vector3 touchPosition, TouchPhase touchPhase) 
+	Vector3 oriTouchPos;
+	private void HandleTouch(int touchFingerId, Vector3 touchPosition, TouchPhase touchPhase)
 	{
-		if((Input.touchCount > 0 || touchFingerId > 0))
+		if((Input.touchCount > 0 || touchFingerId > 0) && bottomwallSc.isBallStickBottom)
 		{
+			touchPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			touchPosition.z = 0f;
+
 			switch (touchPhase)
 			{
 				case TouchPhase.Began:
+					oriTouchPos = touchPosition;
+					line.positionCount = 2;
 				break;
 
 				case TouchPhase.Moved:
+					line.SetPosition(0, ballObj.transform.position);
+					line.SetPosition(1, touchPosition);
+					line.Simplify(0.02f);
 				break;
 				
 				case TouchPhase.Ended:
+					line.positionCount = 0;
+					ballObj.GetComponent<Rigidbody2D>().AddForce(( touchPosition - ballObj.transform.position ) * 100 );
 				break;
 			}
 		}
